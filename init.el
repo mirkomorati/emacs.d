4bab1548c04c453c87304db1a83fdcbfc10d14a1
Mirko Morati <mirkomorati@gmail.com>
2017-03-02 23:51:47 +0100

    TODO: moving every custom to a dedicated file

--- init.el
+++ init.el
@@ -15,9 +15,11 @@
  '(custom-safe-themes
    (quote
     ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" "7b4d9b8a6ada8e24ac9eecd057093b0572d7008dbd912328231d0cada776065a" default)))
+ '(doc-view-continuous t)
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (C . t) (awk . t) (ditaa . t))))
  '(package-selected-packages
    (quote
-    (waher-theme use-package undo-tree solarized-theme phi-search-mc monokai-theme mc-extras haskell-mode guide-key auto-compile))))
+    (org-bullets smartparens waher-theme use-package undo-tree solarized-theme phi-search-mc monokai-theme mc-extras haskell-mode guide-key auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
